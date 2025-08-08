7a2f3423788227dc636dfb471fd738c03ce2f672
Sanat Server <server@sanatci.sanatgezgini.com>
2025-08-08 14:07:14 +0300

    Add : the new command for creating user to login

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    protected $signature = 'user:create {name} {email} {password}';
+    protected $description = 'Create a new user';
+
+    public function handle()
+    {
+        $user = User::create([
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => Hash::make($this->argument('password')),
+        ]);
+
+        $this->info("User {$user->email} created successfully!");
+    }
+}
+
