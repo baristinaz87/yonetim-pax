03748695dc9ca86efe6d1d785d96cdcd007441ee
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2025-08-11 17:59:08 +0300

    Feature: Colored status tags added. Shopify partners link added. UserCharges default order problem fixed. State column order bug fixed.

--- resources/views/livewire/merchant-table.blade.php
+++ resources/views/livewire/merchant-table.blade.php
@@ -1,4 +1,33 @@
-@php use Carbon\Carbon; @endphp
+@php
+    use Carbon\Carbon;
+
+    $statuses = [
+        ""                  => "Durum Seçiniz",
+        "new"               => "Yeni Müşteriler",
+        "active"            => "Aktif Müşteriler",
+        "passive"           => "Pasif Müşteriler",
+        "on_track"          => "Takipteki Müşteriler",
+        "wait_return"       => "Dönüş Beklenenler",
+        "wait_activation"   => "Akt. Bekleyenler",
+        "wait_deactivation" => "Deakt. Bekleyenler"
+    ];
+
+    $colors = [
+        "new"               => "bg-[#2563EB]",
+        "active"            => "bg-[#10B981]",
+        "passive"           => "bg-[#9CA3AF]",
+        "on_track"          => "bg-[#F59E0B]",
+        "wait_return"       => "bg-[#F97316]",
+        "wait_activation"   => "bg-[#3B82F6]",
+        "wait_deactivation" => "bg-[#EF4444]",
+    ];
+
+    $merchants = array_map(function ($item) {
+        $tmp = $item["setting"] ?? [];
+        $tmp["merchantId"] = $item["id"];
+        return $tmp;
+    }, $data)
+@endphp
 <div>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
@@ -52,50 +81,37 @@
             <th></th>
             <th></th>
             <th>
-                <div>
-                    <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5" type="button">
-                        Durum Seç
-                        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                        </svg>
-                    </button>
-                    <div id="dropdownRadio" class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top" style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
-                        <ul class="p-3 space-y-1 text-sm text-gray-700" aria-labelledby="dropdownRadioButton">
-                            @foreach($status as $key => $value)
-                                <li>
-                                    <div class="flex items-center p-2 rounded-sm hover:bg-gray-100">
-                                        <input wire:click="$set('selectedStatus', '{{$key}}')" id="choice-{{$key}}" type="radio" value="{{$key}}" name="filter-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
-                                        <label for="choice-{{$key}}" class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm">{{$value}}</label>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
+                <select id="status" wire:change="$set('selectedStatus', $event.target.value)" class="min-w-[200px] block w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200">
+                    @foreach($statuses as $key => $value)
+                        <option value="{{ $key }}">{{ $value }}</option>
+                    @endforeach
+                </select>
             </th>
             <th></th>
         </tr>
         </thead>
         <tbody>
-        @foreach($data as $customer)
+        @foreach($merchants as $merchant)
             <tr class="bg-white border-b border-gray-200 hover:bg-gray-50">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {{ $customer['setting']['unvan'] ?? '' }}
+                    {{ $merchant['unvan'] ?? '' }}
                 </th>
                 <td class="px-6 py-4">
-                    {{ $customer['setting']['shop_domain'] ?? '' }}
+                    {{ $merchant['shop_domain'] ?? '' }}
                 </td>
                 <td class="px-6 py-4">
-                    {{ !empty($customer['setting']['credit_expired_at']) ? Carbon::parse($customer['setting']['credit_expired_at'])->format('d/m/Y') : '' }}
+                    {{ !empty($merchant['credit_expired_at']) ? Carbon::parse($merchant['credit_expired_at'])->format('d/m/Y') : '' }}
                 </td>
                 <td class="px-6 py-4">
-                    {{ $customer['setting']['credit'] ?? '' }}
+                    {{ $merchant['credit'] ?? '' }}
                 </td>
                 <td class="px-6 py-4">
-                    {{ empty($customer['setting']['status']) ? '' : $status[$customer['setting']['status']] }}
+                    <span class="{{ $colors[$merchant["status"] ?? ""] ?? "bg-gray-300" }} text-white p-2 rounded">
+                        {{ $statuses[$merchant["status"] ?? "empty"] ?? "Durum Boş" }}
+                    </span>
                 </td>
                 <td class="px-6 py-4">
-                    <a href="{{ route('merchant-detail', ['id' => $customer['id']]) }}" class="font-medium text-blue-600 hover:underline">Düzenle</a>
+                    <a href="{{ route('merchant-detail', ['id' => $merchant['merchantId']]) }}" class="font-medium text-blue-600 hover:underline">Düzenle</a>
                 </td>
             </tr>
         @endforeach
