6c07f960c136f000646d955d3e37465e407798a2
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2025-08-15 23:30:36 +0300

    Feature: Dashboard route redirected to the merchant route.

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,11 +30,6 @@ new class extends Component
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('merchants')" :active="request()->routeIs('merchants')" wire:navigate>
                         {{ __('Müşteriler') }}
                     </x-nav-link>
@@ -91,9 +86,6 @@ new class extends Component
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('merchants')" :active="request()->routeIs('merchants')" wire:navigate>
                 {{ __('Müşteriler') }}
             </x-responsive-nav-link>
