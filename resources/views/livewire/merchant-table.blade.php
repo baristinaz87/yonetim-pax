d6eb7f718362ece34c1b1a722c8bd7f18d5d01a7
Tamer Erdoğan <tamer.erdogaan@gmail.com>
2025-08-23 13:29:37 +0300

    Feature: Merchant table row hover style changed.

--- resources/views/livewire/merchant-table.blade.php
+++ resources/views/livewire/merchant-table.blade.php
@@ -101,7 +101,7 @@
         </thead>
         <tbody>
         @foreach($merchants as $merchant)
-            <tr class="bg-white border-b border-gray-200 hover:bg-gray-50">
+            <tr class="bg-white border-b border-gray-200 hover:bg-gray-200">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                     {{ $merchant['unvan'] ?? '' }}
                 </th>
